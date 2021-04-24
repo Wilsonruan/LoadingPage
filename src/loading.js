5359d1b4e0733f57a830c2addc82c880bdc8756b
Wilson Ruan <wilsonruan23@gmail.com>
2021-04-24 19:42:58 -0400

    inits home page

--- src/loading.js
+++ src/loading.js
@@ -2,7 +2,8 @@ import React from "react";
 import Lottie from "react-lottie";
 import "bootstrap/dist/css/bootstrap.css";
 import * as legoData from "./legoloading.json";
-import App from './App';
+import Main from './pages/main'
+import { Container } from "react-bootstrap";
 
 const defaultOptions = {
   loop: true,
@@ -16,21 +17,14 @@ const defaultOptions = {
 export default class Loading extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      done: undefined
-    };
+    this.state = {isStopped: false, isPaused: false};
   }
 
   componentDidMount() {
     setTimeout(() => {
       fetch("https://jsonplaceholder.typicode.com/posts")
         .then(response => response.json())
-        .then(json => {
-          this.setState({ loading: true });
-          setTimeout(() => {
-            this.setState({ done: true });
-          }, 2000);
-        });
+        .then(json => this.setState({ done: true }));
     }, 1200);
   }
 
@@ -38,12 +32,14 @@ export default class Loading extends React.Component {
     return (
       <div>
         {!this.state.done ? (
-            <div style={{ textAlign: 'center', marginTop: '300px', fontSize: '48px' }}>
+            <div style={{ textAlign: 'center', marginTop: '300px', fontSize: '48px', marginBottom: '600px'  }}>
               <h1>Fetching Data</h1>
-              <Lottie options={defaultOptions} height={120} width={120} />                      
+              <Lottie options={defaultOptions} height={120} width={120} isStopped={this.state.isStopped} isPaused={this.state.isPaused}/>                      
             </div>
         ) : (
-            <App />
+          <Container>
+            <Main />
+          </Container>
         )}
       </div>
     );
---
+++ src/pages/home.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Alert, Figure, Container, Col, Row } from "react-bootstrap"; 
+import Wilson from "./Wilson-Ruan.jpg"
+
+class home extends Component {
+  render() {
+    return (
+      <div>
+        <Container style={{ background: "white", marginTop: "160px", padding: "35px", boxShadow: '5px 10px 18px #888888' }}>
+          <Alert>
+          <Figure.Image style={{ margin: "35px" }} width={250} height={250} alt="Wilson Ruan" src={Wilson} className="float-right" rounded />
+            <h1>Hello there,</h1>
+            <p className="paragraphText">I am a Full Stack Developer with a passion for new ideas, visually engaging, and fully responsive website. Programming allows me to engage both the creative and logical sides of my brain, and I love the feeling of bringing new ideas to coding.</p>
+            <p className="paragraphText">My skills include JavaScript, ES6, jQuery, React/React Hooks, Redux, React Native, Node, Express, MySQL, Sequelize.js MongoDB, Mongoose.js, REST APIs, HTML5, Handlebars, CSS3, Bootstrap, Material UI, Git, and GitHub.</p>
+            <p className="paragraphText">I am constantly learning new techniques and collaborating with other developers to become a better Full Stack Developer.</p>
+            <hr />
+            <Row>
+              <Col sm><a href="https://drive.google.com/file/d/197RK4KqoGBqjp0ypGt3cPX-lBromFwWn/view?usp=sharing" target="_blank" rel="noreferrer">Click here to view my Resume</a></Col>
+              <Col sm><a className="float-right" href="mailto:wilsonruan23@gmail.com" target="_blank" rel="noreferrer">Email: wilsonruan23@gmail.com</a></Col>
+            </Row>  
+          </Alert>
+        </Container>
+      </div>
+    )
+  }
+}
+
+export default home;
